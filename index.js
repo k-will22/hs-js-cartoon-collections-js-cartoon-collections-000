9f3f1d911e138cbdc354aae73c04344dcd509de1
Keith Williams <thestreetkw@hotmail.com>
2020-10-01 19:22:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 function dwarfRollCall(dwarves) {
+  var dwarfArray = [];
+  for (var i = 0; i < dwarves.length; i++) {
+    dwarfArray.push(`${i+1}. ${dwarves[i]} `);
+  }
+     return dwarfArray.join("");
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var callsArray = [];
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    callsArray.push(`${planeteerCalls[i].toUpperCase()}!`); 
+   }
+     return callsArray;
 }
 
 function longPlaneteerCalls(words) {
+  for(var i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+      }
+    }
+      return false;
 }
 
 function findTheCheese (foods) {
-}
+  for (var i = 0; i < foods.length; i++) {
+    if (foods[i] === "cheddar" ||
+        foods[i] === "gouda" ||
+        foods[i] === "camembert") {
+      return foods[i];
+      }
+    }
+      return "no cheese!";
+  } 
